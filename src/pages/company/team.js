d873f45a5dab828c87b19ec1338d8d0e31b91730
Cam <farquhc@mcmaster.ca>
2019-07-22 16:02:35 -0400

    .

---
+++ src/pages/company/team.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../../components/layout"
+
+const Team = () => (
+  //   <Layout>
+  //     <SEO title="Page two" />
+  //     <h1>Hi from the second page</h1>
+  //     <p>Welcome to page 2</p>
+  //     <Link to="/">Go back to the homepage</Link>
+  //   </Layout>
+  <Layout>
+    <div>
+      <h1>The Team</h1>
+      <p>Well, if there's a bright center to the universe, you're on the planet that it's farthest from. I see, sir. Uh, you can call me Luke. I see, sir Luke. Just Luke. And I am See-Threepio, human-cyborg relations, and this is my counterpart, Artoo-Detoo. Hello. You got a lot of carbon scoring here. It looks like you boys have seen a lot of action. With all we've been through, sometimes I'm amazed we're in as good condition as we are, what with the Rebellion and all. You know of the Rebellion against the Empire? That's how we came to be in your service, if you take my meaning, sir.</p>
+    </div>
+  </Layout>
+)
+
+export default Team
